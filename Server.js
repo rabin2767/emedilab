d3c4836569d6a23894a5e3bd5f07560044e36262
Rabin Chacko <ravarghe@cisco.com>
2016-12-09 13:13:49 +0530

    Initial Commit

---
+++ Server.js
@@ -0,0 +1,58 @@
+var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+
+/*
+	Here we are configuring our SMTP Server details.
+	STMP is mail server which is responsible for sending and recieving email.
+*/
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'medicare247blr@gmail.com',
+        pass: 'medicare'
+    },
+    tls: {rejectUnauthorized: false},
+    debug:true
+});
+
+
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+var callback = function(req,res){
+    res.sendFile(__dirname+'/index2.html');
+};
+var callback2 = function(req,res){
+    res.sendFile(__dirname+'/index.html');
+};
+
+app.get('/new',callback);
+app.get('/',callback2);
+app.use("/assets",express.static(__dirname + "/assets"));
+app.get('/send',function(req,res){
+	var mailOptions={
+		to : req.query.to,
+		subject : req.query.subject,
+		text : req.query.text
+	}
+	console.log(mailOptions);
+	smtpTransport.sendMail(mailOptions, function(error, response){
+   	 if(error){
+        	console.log(error);
+		res.end("error");
+	 }else{
+        	console.log("Message sent: " + response.message);
+		res.end("sent");
+    	 }
+});
+});
+
+/*--------------------Routing Over----------------------------*/
+
+app.listen(3000,function(){
+	console.log("Express Started on Port 3000");
+});
